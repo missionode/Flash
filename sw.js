340a3b0ce264270d07c04a013e8480aa4c0ee9a2
missionode <116753080+missionode@users.noreply.github.com>
2024-11-20 18:32:47 +0530

    Add files via upload
    Update sw

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ var cacheName = 'flash-pwa';
 
 var filesToCache = [
   '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '../index.html',
+  '../css/style.css',
+  '../js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
